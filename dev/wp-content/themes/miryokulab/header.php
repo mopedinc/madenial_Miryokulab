976ffbf90767fd39e3679d564b1b941f99023323
藤倉 <fujikura@moped.co.jp>
2023-10-16 20:13:33 +0900

    meta設定

--- dev/wp-content/themes/miryokulab/header.php
+++ dev/wp-content/themes/miryokulab/header.php
@@ -3,10 +3,6 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>地域と文化の魅力ラボ</title>
-  <meta name="description" content="">
-  <link rel="icon" href="https://miryoku-lab.com/assets/img/fabicon.ico" type="image/png">
-  <meta property="og:image" content="https://miryoku-lab.com/assets/img/ogp.png">
   <link href="<?php echo get_template_directory_uri(); echo '/assets/css/style.css?' . filemtime( get_stylesheet_directory() . '/assets/css/style.css'); ?>" rel="stylesheet" type="text/css">
   <?php wp_head(); ?>
 </head>
